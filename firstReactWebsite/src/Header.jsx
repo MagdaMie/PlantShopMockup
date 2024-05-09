a3082447032ffc6865fb53fe1c8f5f1c5b1ec0dc
MagdalenaMie <madeladesign@gmail.com>
2024-05-09 16:26:38 +0300

    initial commit

---
+++ firstReactWebsite/src/Header.jsx
@@ -0,0 +1,22 @@
+
+
+const Header = () => {
+
+    return(
+    <div className="header">
+        <div className="logo"><img src="/MadelaLogo.png" alt="" /></div>
+        <div className="nav-bar">
+            <ul>
+                <li>SKLEP</li>
+                <li>O MNIE</li>
+                <li>KONTAKT</li>
+                <li>FAQ</li>
+                <li>KOSZYK</li>
+            </ul>
+        </div>
+   </div>
+    )
+
+}
+
+export default Header
