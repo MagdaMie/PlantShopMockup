5c62295620c5d4f87e6a4f21b3afc906e52ffa79
MagdalenaMie <madeladesign@gmail.com>
2024-05-12 00:49:17 +0300

    created ProductPage

--- firstReactWebsite/src/App.jsx
+++ firstReactWebsite/src/App.jsx
@@ -1,5 +1,8 @@
-import FaqPage from "./components/FaqPage"
 import Header from "./components/Header"
+import ProductPage from "./components/ProductPage"
+import AboutMe from "./components/AboutMe"
+import Products from "./components/Products"
+import FaqPage from "./components/FaqPage"
 
 
 function App() {
@@ -7,6 +10,9 @@ function App() {
   return (
     <>
       <Header />
+      <AboutMe />
+      <Products />
+      <ProductPage />
       <FaqPage />
     </>
   )
---
+++ firstReactWebsite/src/components/ProductPage.jsx
@@ -0,0 +1,17 @@
+
+
+const ProductPage = () => {
+
+    return(
+    <div className="product-page">
+        <div className="product-display">
+                <img src="/9.png" alt="" />
+        </div>
+        <div className="product-description">
+            
+        </div>
+    </div>
+    )
+}
+
+export default ProductPage
