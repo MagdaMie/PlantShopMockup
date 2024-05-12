5c837a17a266cb1b5e30dff183d289939663d30d
MagdalenaMie <madeladesign@gmail.com>
2024-05-12 19:53:27 +0300

    finished ProductPage

--- firstReactWebsite/src/components/Hero.jsx
+++ firstReactWebsite/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
                 <img src="/hero.jpg" alt="" />
             </div>
             <div className="about-wrap">
-                <h3>get to know me</h3>
+                <h3>get to know us</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ipsa iusto sint pariatur fuga tenetur! Placeat fugiat sapiente consequatur blanditiis iure ratione? Excepturi natus ut quidem, architecto pariatur laboriosam, modi repellendus minus, molestiae deserunt ipsam doloremque praesentium placeat dolore ipsa quo quod odit voluptatem necessitatibus! Ipsa repellat iste quisquam porro.</p>
             </div>
         </div>
--- firstReactWebsite/src/components/ProductPage.jsx
+++ firstReactWebsite/src/components/ProductPage.jsx
@@ -1,4 +1,4 @@
-
+import ButtonBetter from "./ButtonBetter"
 
 const ProductPage = () => {
 
@@ -8,8 +8,38 @@ const ProductPage = () => {
                 <img src="/9.png" alt="" />
         </div>
         <div className="product-description">
-            
+            <div className="wrap">
+                <h3>Plant name</h3>
+                <p className="price">$45</p>
+                <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consectetur eaque dolores, quisquam inventore debitis fugiat! Libero natus dolorum fugit, odio ipsum harum aperiam, dicta et facilis omnis sunt deserunt, assumenda cumque ad sint voluptatem eius excepturi quis labore ipsam repellendus perferendis sed delectus! Nisi quibusdam odit nostrum nam sapiente velit? Blanditiis consequuntur earum perspiciatis illo aut </p>
+
+                <div className="product-care">
+                    <ul>
+                        <li>
+                            <img src="/icon5.png" alt="" />
+                            <p>Lorem, ipsum</p>
+                        </li>
+                        <li>
+                            <img src="/icon5.png" alt="" />
+                            <p>Lorem, ipsum</p>
+                        </li>
+                        <li>
+                            <img src="/icon5.png" alt="" />
+                            <p>Lorem, ipsum</p>
+                        </li>
+                        <li>
+                            <img src="/icon5.png" alt="" />
+                            <p>Lorem, ipsum</p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                <ButtonBetter 
+                content={'Add to the cart'}/>
+                </div>
+            </div>
         </div>
+
     </div>
     )
 }
