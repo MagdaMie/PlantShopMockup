7c070984437afcde2baf10bfb0acea3680ad4352
MagdalenaMie <madeladesign@gmail.com>
2024-12-24 19:21:08 +0200

    updated vite.config.js

--- firstReactWebsite/vite.config.js
+++ firstReactWebsite/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  build: {
+    cssCodeSplit: true, // Ensures CSS is split into a separate file
+    minify: 'esbuild', // Optional: Use esbuild for minifying production builds
+  },
 })
