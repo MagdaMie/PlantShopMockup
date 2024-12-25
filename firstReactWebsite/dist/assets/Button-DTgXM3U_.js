48e626726aae38624a0b77748f85a4719e8a851f
MagdalenaMie <madeladesign@gmail.com>
2024-12-25 20:40:56 +0200

    updated basename in BrowserRouter

---
+++ firstReactWebsite/dist/assets/Button-DTgXM3U_.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Cozy1Tw1.js";const u=({content:e,onClick:t,disable:r,type:o})=>s.jsx("button",{className:"bg-customGreen-dark px-[18px] py-2 rounded-3xl hover:bg-customGreen-default text-center",onClick:t,disabled:r,type:o,children:e});export{u as B};
