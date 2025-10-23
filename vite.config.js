650d0888b3f7cc22386eb7a4cd4e2dcc59273773
MagdalenaMie <madeladesign@gmail.com>
2025-10-23 14:03:18 +0200

    chore: add vite-imagetools plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { imagetools } from "vite-imagetools";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+  plugins: [react(), imagetools()],
+  base: "/",
   build: {
     cssCodeSplit: true, // Ensures CSS is split into a separate file
-    minify: 'esbuild', // Optional: Use esbuild for minifying production builds
+    minify: "esbuild", // Optional: Use esbuild for minifying production builds
   },
-})
+});
